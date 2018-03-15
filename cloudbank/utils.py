b2ec868b8503fb7281b82d3481550467bce0f2a2
Yasin Aktimur <chardone@windowslive.com>
2018-03-16 00:47:29 +0300

    01010101

--- cloudbank/utils.py
+++ cloudbank/utils.py
@@ -155,7 +155,7 @@ def checktimepass():
         else:
             return False
 
-    elif gethours == 23:
+    elif gethours == 1:
         if getminute <= 30:
             return True
         else:
