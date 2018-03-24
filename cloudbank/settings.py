14876702a89324bda890ee238b642e1d84b138b2
Yasin Aktimur <chardone@windowslive.com>
2018-03-25 00:14:09 +0300

    some test.s

--- cloudbank/settings.py
+++ cloudbank/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-
+import django
+django.setup()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
