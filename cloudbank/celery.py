cee76e20c8c9b2e918a5072aa9ef602d57d0dba8
Yasin Aktimur <chardone@windowslive.com>
2018-03-25 00:25:07 +0300

    asd

--- cloudbank/celery.py
+++ cloudbank/celery.py
@@ -15,24 +15,7 @@ app = Celery('cloudbank')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django app configs.
-app.autodiscover_tasks()
-
-
-@app.on_after_configure.connect
-def setup_periodic_tasks(sender, **kwargs):
-    # Calls test('hello') every 10 seconds.
-    sender.add_periodic_task(2.0, test.s('hello'), name='add every 10')
-
-    # Calls test('world') every 30 seconds
-    sender.add_periodic_task(3.0, test.s('world'), expires=10)
-
-    # Executes every Monday morning at 7:30 a.m.
-    sender.add_periodic_task(
-        crontab(hour=7, minute=30, day_of_week=1),
-        test.s('Happy Mondays!'),
-    )
-
-
+#app.autodiscover_tasks()
 
 
 @app.task(bind=True)
