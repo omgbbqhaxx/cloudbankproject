77c9246251d34c6a69db8c8bd14cc3fbc7bae651
Yasin Aktimur <chardone@windowslive.com>
2018-03-25 00:11:17 +0300

    celery 2

--- core/models.py
+++ core/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+import django
+django.setup()
 from django.db import models
 from django.utils import timezone
 import pytz, datetime
