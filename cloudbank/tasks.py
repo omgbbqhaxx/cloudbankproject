b12c4b5c978ebb358e51dccdf38b8662f5423ddc
Yasin Aktimur <chardone@windowslive.com>
2018-03-25 02:28:44 +0300

    tests

--- cloudbank/tasks.py
+++ cloudbank/tasks.py
@@ -1,13 +1,13 @@
 # Create your tasks here
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
-from cloudbank.utils import checkreward
-
-
-@shared_task
-def givereward(x, y):
-    print("im here bro")
-    return checkreward()
+# from cloudbank.utils import checkreward
+#
+#
+# @shared_task
+# def givereward(x, y):
+#     print("im here bro")
+#     return checkreward()
 
 @shared_task
 def add(x, y):
